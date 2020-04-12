9360617f281c565442bf35d4093f9dbba2a57ad0
kiranrlohar <kiranlohar0909@gmail.com>
2020-04-12 21:02:20 +0530

    Re-Commiting assignment on List date 12/04/2020

--- D12042020/Kiran/ListAssignement.py
+++ D12042020/Kiran/ListAssignement.py
@@ -14,21 +14,31 @@ for x in vehicles:
     if x == 'Tiago' or x == 'Harrier':
         if 'TATA' in manufacturers:
             print(x + ' is a \'TATA\' vehicle')
+        else:
+            print("Manufacturer TATA is not present in the Manufacturers List")
     if x == 'Seltos':
         if 'KIA' in manufacturers:
             print(x + ' is a KIA vehicle')
+        else:
+            print("Manufacturer KIA is not present in the Manufacturers List")
     if x == 'Creta' or x == 'i20':
         if 'HYUNDAI' in manufacturers:
             print(x + ' is a HYUNDAI vehicle')
+        else:
+            print("Manufacturer HYUNDAI is not present in the Manufacturers List")
     if x == 'Innova' or x == 'Fortuner':
         if 'TOYOTA' in manufacturers:
             print(x + ' is a TOYOTA vehicle')
+        else:
+            print("Manufacturer TOYOTA is not present in the Manufacturers List")
     if x == 'Kwid' or x == 'Duster':
         if 'RENAULT' in manufacturers:
             print(x + ' is a RENAULT vehicle')
-            
+        else:
+            print("Manufacturer RENAULT is not present in the Manufacturers List")
 
 # 2. Find the second largest number in the list.
+# Testing done with below various numbers list
 #numbers = [2, 4, 1, 6, 5, 8]
 #numbers = [4, 4, 1, 5, 8]
 #numbers = [4, 4]
