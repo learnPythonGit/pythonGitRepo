96e9048a6b13a0d17f0c0f7214a6ed9ee031e39e
Saurav Kumar <Saurav.Kumar2@coxautoinc.com>
2020-04-17 01:04:56 +0530

    Added some change for a script

--- Assignments/20200413_Control_Flow/secondLargeNumSaurav.py
+++ Assignments/20200413_Control_Flow/secondLargeNumSaurav.py
@@ -7,15 +7,23 @@
 # ...................................
 
 # Compare two number and find out the greater number (using if else)
-
-lst = [3, 2, 7, 4, 6, 6, 5]
+print("Assignment : Find second largest of the given numbers in list - Method 1")
+lst = [3, 2, 7, 4, 6, 6, 5, 8]
 # sorting of list
 newList = []
 for i in lst:
     if i not in newList:
         newList.append(i)
 newList.sort(reverse=True)
-print(newList)
 print("Second Largest number is {0}".format(newList[1]))
+
+# FInd the second Largest number from the list of Number- Ashish
+
+print("Assignment : Find second largest of the given numbers in list - Method 2")
+number_list = [1,2,3,4,2,1,5,6,8,4,1,5,1,5,4,8]
+number_list = list(set(number_list))
+number_list.sort(reverse=True)
+print("Second Largest number is {0}".format(number_list[1]))
+print("\n" + "-"*80)
 
 
